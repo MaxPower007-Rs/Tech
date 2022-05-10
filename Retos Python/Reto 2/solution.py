c0448f7c2ea78919bac816df1936c31d122cee29
MaxPower007-Rs <56212392+MaxPower007-Rs@users.noreply.github.com>
2022-05-10 15:47:47 -0500

    Files

---
+++ Retos Python/Reto 2/solution.py
@@ -0,0 +1,14 @@
+# POR FAVOR LEA TODAS LAS INDICACIONES SUMINISTRADAS EN EL 
+# ENUNCIADO ANTES DE EMPEZAR A IMPLEMENTAR SU SOLUCIÓN
+
+# NO MODIFICAR EL NOMBRE, PARÁMETROS O RETORNO DE LA FUNCIÓN
+def modificar_materia(pensum, semestre, materia, nombre, creditos):
+    # ACÁ INICIA LA FUNCIÓN modificar_materia (En este espacio debes 
+    # poner el código necesario para implementar esta funcionalidad)
+    
+    
+    
+    # ACÁ TERMINA LA FUNCIÓN modificar_materia
+    # NO MODIFIQUES LA SIGUIENTE LÍNEA
+    return mensaje
+
