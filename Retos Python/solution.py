f14d8cd1af92dfefdcb9f08a272ee9c7952cb704
MaxPower007-Rs <56212392+MaxPower007-Rs@users.noreply.github.com>
2022-05-10 15:43:23 -0500

    Add files via upload

---
+++ Retos Python/solution.py
@@ -0,0 +1,29 @@
+"""
+NO ELIMINAR LAS SIGUIENTES IMPORTACIONES, 
+El equipo de desarrollo de este calificador modificó las funciones 'print' e 'input'.
+Esta modificación se hizo con la finalidad de que el sistema pueda calificar tu solución.
+Por eso LEER MUY BIEN LO QUE SE SOLICITA Y LAS RESTRICCIONES QUE SE LE IMPUSIERON A ESTAS DOS FUNCIONES.
+"""
+from student_utilities import input, print
+
+
+def solucion():
+    #ACÁ INICIA LA FUNCIÓN SOLUCIÓN (En este espacio debes entregar tu solución)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    #ACÁ TERMINA LA FUNCIÓN SOLUCIÓN
+    
+"""
+¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE! ¡IMPORTANTE!
+NO AÑADIR CÓDIGO FUERA DE LA FUNCIÓN solucion().
+SOLO AÑADIR CÓDIGO ENTRE EL ESPACIO DONDE DICE: ACÁ INICIA... ACÁ TERMINA
+"""
