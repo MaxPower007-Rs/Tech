43333cb8a34ece3de5c9fbb6ef2934d36092c5bc
MaxPower007-Rs <56212392+MaxPower007-Rs@users.noreply.github.com>
2022-06-03 15:13:49 -0500

    Update solucion.py

--- Retos Python/Reto 4/solucion.py
+++ Retos Python/Reto 4/solucion.py
@@ -9,40 +9,46 @@ pensum = [{'0123':{'créditos':2, 'nombre':'intro a la ing'},
           {},
           {}]
 
+# POR FAVOR LEA TODAS LAS INDICACIONES SUMINISTRADAS EN EL 
+# ENUNCIADO ANTES DE EMPEZAR A IMPLEMENTAR SU SOLUCIÓN
+
 def es_semestre_valido(p, s):
+    # ESTA VEZ TU DEFINES TUS RETORNOS
     # Pensum / Semestre
     # Semestre empieza en 1, y la ista en 0
     s_qty = len(p)
-    s-=1                                          # Logitud de la lista, igual a cantidad de semestres
+    #s-=1                                         # Logitud de la lista, igual a cantidad de semestres
     if s <= 0 or s > s_qty:                                 # Si el semestre es menor a 0 o mayor a la cantidad, retorna False
         isTrue = False
     else:
         isTrue = True
     return isTrue 
-
+    
 def es_semestre_vacio(p, s):
+    # ESTA VEZ TU DEFINES TUS RETORNOS
+    s-=1
     isEmpty = True if p[s]=={} else False
     return isEmpty
-
+    
 def es_materia_valida(p, s, m):
-    s-=1
+    # ESTA VEZ TU DEFINES TUS RETORNOS
+    courseOK = False
     materias = p[s]
     for d in materias:
         course = materias[d]['nombre']
-        if course==m:            
+        if course!=" ":            
             courseOK = True
-        else: 
-            courseOK = False 
-            break
+            break          
     return courseOK
-        
-def modificar_materia(pensum , semestre, materia, nombre, creditos):
+    
+def modificar_materia(pensum, semestre, materia, nombre, creditos):
+    # ACÁ INICIA LA FUNCIÓN
     p = pensum
     s = semestre 
     m = materia 
     n = nombre 
     c = creditos
-    msg =''
+    #msg =''
     s-=1
     
     
@@ -50,16 +56,21 @@ def modificar_materia(pensum , semestre, materia, nombre, creditos):
         if not es_semestre_vacio(p, s):
             if es_materia_valida(p, s, m):
                 p[s][m]['nombre'] = n
-                p[s][m]['créditos'] = creditos
-                msg = 'Course update done'
+                p[s][m]['créditos'] = c
+                msg = True
             else:
-                msg = "Course don't exist"
+                msg = False
         else:
-            msg = 'Semester is empty'
+            msg = False
     else: 
-        msg: 'Semester is wrong'
+        msg = False    
+    
+    
+    # ACÁ TERMINA LA FUNCIÓN
+    # ESTA VEZ TU DEFINES TUS RETORNOS
 
 def eliminar_materia(pensum, semestre, materia):
+    # ACÁ INICIA LA FUNCIÓN
     p = pensum
     s = semestre 
     m = materia 
@@ -71,14 +82,13 @@ def eliminar_materia(pensum, semestre, materia):
                 x = p[s]
                 x.pop(m)
                 
-                msg = 'done'
+                msg = True
             else:
-                msg = "Course don't exist"
+                msg = False
         else:
-            msg = 'Semester is empty'
+            msg = False
     else: 
-        msg ='Semester is wrong'
+        msg = False
     
-    return msg
-    
-
+    # ACÁ TERMINA LA FUNCIÓN
+    # ESTA VEZ TU DEFINES TUS RETORNOS
